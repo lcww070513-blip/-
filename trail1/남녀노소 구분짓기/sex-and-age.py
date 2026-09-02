d8b115af440d17afd9131d75cbff79b924995b4b
lcw070513 <lcww070513@gmail.com>
2026-09-02 15:32:11 +0900

    [Passed] 남녀노소 구분짓기 (51ms, 16MB)

---
+++ trail1/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+if a == 0 and  19>b :
+    print("BOY")
+elif a == 1 and 19>b :
+    print("GIRL")
+elif a == 0 and  b>=19 :
+    print("MAN")
+elif a == 1 and b>=19 :
+    print("WOMAN")
