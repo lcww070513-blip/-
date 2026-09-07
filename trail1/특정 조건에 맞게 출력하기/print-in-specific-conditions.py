c3a09f8335801dfe5749bd0568e779757214972f
lcw070513 <lcww070513@gmail.com>
2026-09-07 23:09:45 +0900

    [Passed] 특정 조건에 맞게 출력하기 (47ms, 16MB)

---
+++ trail1/특정 조건에 맞게 출력하기/print-in-specific-conditions.py
@@ -0,0 +1,14 @@
+n = list((map(int,input().split())))
+
+for i in n:
+    if i == 0:
+        break
+
+    if i % 2 == 0:
+        print(i // 2,end=" ")
+    else:
+        print(i + 3,end=" ")
+
+
+        
+
