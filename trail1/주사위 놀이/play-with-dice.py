d399fc39c61b592489c373dcf2c272b3d0aefb9f
lcw070513 <lcww070513@gmail.com>
2026-09-07 23:23:59 +0900

    [Passed] 주사위 놀이 (48ms, 16MB)

---
+++ trail1/주사위 놀이/play-with-dice.py
@@ -0,0 +1,22 @@
+item_cnt = {
+    1:0,2:0,3:0,4:0,5:0,6:0
+}
+n = list((map(int,input().split())))
+for i in range(len(n)):
+    item_cnt[n[i]] +=1
+for num, cnt in item_cnt.items():
+    print(num, "-", cnt)
+
+
+# item_cnt = {
+#     1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0
+# }
+# n = int(input())
+# lst=  list(map(int,input().split()))
+
+# for i in range(len(lst)):
+#     item_cnt[lst[i]] +=1
+
+
+# for cnt in item_cnt.values():
+#     print(cnt)
