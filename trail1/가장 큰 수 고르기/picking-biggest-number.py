9bfb9f6bd72c14e48b9090c0ba655793eddfebba
lcw070513 <lcww070513@gmail.com>
2026-09-09 14:29:43 +0900

    [Passed] 가장 큰 수 고르기 (57ms, 16MB)

--- trail1/가장 큰 수 고르기/picking-biggest-number.py
+++ trail1/가장 큰 수 고르기/picking-biggest-number.py
@@ -1,2 +1,19 @@
-M_list = list(map(int,input().split()))
-print(max(M_list))
+# M_list = list(map(int,input().split()))
+# max_num = M_list[0]
+# for i in range(len(M_list)):
+#     if i > max_num:
+#         max_num = i
+       
+#     print(max_num)
+
+
+
+arr = list(map(int,input().split()))
+max_num = arr[0]
+for i in range(len(arr)):
+    if arr[i] > max_num:
+        max_num = arr[i]
+print(max_num)
+
+    
+    
